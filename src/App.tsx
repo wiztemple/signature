841f18816134dac51bfcefed9cc9b7e6a3c679b2
chris097 <chrisfidel.international@gmail.com>
2023-10-04 14:36:28 +0100

    personal page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Resources from "./pages/Resources";
-import PersonalBanking from "./pages/PersonalBanking";
-import PrivateBanking from "./pages/PrivateBanking";
-import Business from "./pages/Business";
-import InstitutionalBanking from "./pages/InstitutionalBanking";
-import DigitalBanking from "./pages/DigitalBanking";
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/personal-banking" element={<PersonalBanking />} />
-          <Route path="/private-banking" element={<PrivateBanking />} />
-          <Route path="/business-banking" element={<Business />} />
-          <Route path="/institutional-banking" element={<InstitutionalBanking />} />
-          <Route path="/digital-banking" element={<DigitalBanking />} />
-          <Route path="/resources" element={<Resources />} />
-        </Routes>
-      </BrowserRouter>
+      <div className='font-nunito'>
+      <Footer />
+      </div>
     </>
   );
 }
