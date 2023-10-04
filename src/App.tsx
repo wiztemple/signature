90b8553d2819450a52c80d339a0ef5aa73e79635
chris097 <chrisfidel.international@gmail.com>
2023-10-04 15:13:54 +0100

    personal banking

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Footer from './components/Footer'
-import PersonalBanking from './pages/PersonalBanking'
 
 function App() {
   return (
     <>
       <div className='font-nunito'>
-      <PersonalBanking />
       <Footer />
       </div>
     </>
