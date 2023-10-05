f5f6b07b44072024bc97aafd547e3a96d2d60cf6
chris097 <chrisfidel.international@gmail.com>
2023-10-05 15:32:05 +0100

    git conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import Footer from './components/Footer'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Resources from "./pages/Resources";
+import PersonalBanking from "./pages/PersonalBanking";
+import PrivateBanking from "./pages/PrivateBanking";
+import Business from "./pages/Business";
+import InstitutionalBanking from "./pages/InstitutionalBanking";
+import DigitalBanking from "./pages/DigitalBanking";
 
 function App() {
   return (
     <>
-      <div className='font-nunito'>
-      <Footer />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/personal-banking" element={<PersonalBanking />} />
+          <Route path="/private-banking" element={<PrivateBanking />} />
+          <Route path="/business-banking" element={<Business />} />
+          <Route path="/institutional-banking" element={<InstitutionalBanking />} />
+          <Route path="/digital-banking" element={<DigitalBanking />} />
+          <Route path="/resources" element={<Resources />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
