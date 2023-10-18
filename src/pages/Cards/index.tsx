6845df263b19a6200d89a34581ca499f67b6ca29
chris097 <chrisfidel.international@gmail.com>
2023-10-18 12:02:45 +0100

    card faqs updates

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -70,10 +70,8 @@ const nairaCard: { title: string, description: string[] }[] = [
     description: [
       `Enjoy full control of your account with Signature Verve Naira Debit cards. Our debit card is linked to your
 Savings or Current account, and allows you to make payments on WEB, POS, and use on the ATM`,
-      // "Account maintenance charge (₦1/mille)",
-      // "Access to e-banking services.",
       `FEATURES:`,
-`The following exciting features are available on the signature verve naira debit card.
+      `The following exciting features are available on the signature verve naira debit card.
 - Free POS/WEB transactions
 - ATM cash withdrawal and funds transfer
 - Bills payment
@@ -162,6 +160,43 @@ const faqs: { title: string, description: string[] }[] = [
     title: "What is the validity period?",
     description: [
       `4 years:`
+    ],
+  },
+  {
+    title: "What do I do if I forget my card PIN?",
+    description: [
+      `You can retrieve your card details on the Signature Mobile app. You can also send an email to customercare@signaturebankng.com or walk into any Signature Bank branch to place pin retrieval request.`
+    ],
+  },
+  {
+    title: "What are the transaction charges on Signature Verve Naira Card?",
+    description: [
+      `- Free on all Signature Bank ATMs; however, a ₦35 charge is applicable per withdrawal after your 3rd withdrawal on other Banks’ ATM within a month.`,
+      "- Free on POS and WEB."
+    ],
+  },
+  {
+    title: "Is Signature Bank Verve cards secured?",
+    description: [
+      "Yes, it is secured with sophisticated PIN and Chip technology. These are in addition to the other security measures that have been put in place."
+    ],
+  },
+  {
+    title: "How many Verve naira cards can I have at a time under my savings/current account?",
+    description: [
+      "You can only have one verve naira card on your savings/current account at any time. "
+    ],
+  },
+  {
+    title: "What do I do if my card gets stolen or misplaced?",
+    description: [
+      "You can hotlist your card via Signature Mobile app. You can call our Contact Centre on +2349130000284-6, send an email to customercare@signaturebankng.com or visit any Signature Bank branch for assistance."
+    ],
+  },
+  {
+    title: "Office Address",
+    description: [
+      "Signature Bank Head office and Abuja Branch:  31B Gana Street, Maitama, Abuja FCT, Nigeria."
     ],
   },
 ];
@@ -389,7 +424,7 @@ const Cards = () => {
                     </div>
                     {(onFaq === true && activeIndex === index) && (<div className="flex flex-col gap-3 my-3 pl-10 pr-5">
                       {faq.description.map((faq, index) => (
-                        <div key={index} className="flex items-start w-[55%] gap-1">
+                        <div key={index} className="flex items-start w-[93%] gap-1">
                           {/* <img src={tickSvg} alt="" /> */}
                           <p className="text-base font-nunitoSans font-bold text-hex-9/80">{faq}</p>
                         </div>
